45052b5a0690200609c3688888623d0918ba2f5d
leolleocomp <leolleo.comp@gmail.com>
2023-03-06 21:20:42 +0000

    Fix wrong run on normal pipelines

--- .github/scripts/gen_infos.sh
+++ .github/scripts/gen_infos.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+ref_name="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME}}"
+
 # Taken from https://github.com/rlespinasse/slugify-value/blob/v1.x/slugify.sh
 ref_name_slug=$(\
-  echo ${GITHUB_HEAD_REF} \
+  echo ${ref_name} \
     | sed -E -e 's#refs/[^\/]*/##;s/[^a-zA-Z0-9._-]+/-/g;s/^-*//' -e 's/-*$//' \
 )
 
