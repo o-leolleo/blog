b221a1f6f2517c7658ede148709a2200f21e9f0a
leolleocomp <leolleo.comp@gmail.com>
2023-03-06 20:06:05 +0000

    Fix var name

--- .github/scripts/gen_infos.sh
+++ .github/scripts/gen_infos.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
 # Taken from https://github.com/rlespinasse/slugify-value/blob/v1.x/slugify.sh
-slug=$(\
+ref_name_slug=$(\
   echo ${GITHUB_REF_NAME} \
     | sed -E -e 's#refs/[^\/]*/##;s/[^a-zA-Z0-9._-]+/-/g;s/^-*//' -e 's/-*$//' \
 )
 
-echo "infos=${slug}" >> "${GITHUB_OUTPUT}"
+echo "infos=${ref_name_slug}" >> "${GITHUB_OUTPUT}"
 
-if [ "${slug}" = "main" ]; then
+if [ "${ref_name_slug}" = "main" ]; then
   env_name="${LIVE_ENV_NAME}"
   url="${LIVE_ENV_URL}"
 else
-  env_name="${slug}"
-  url="https://${slug}.${PREVIEW_ENV_DOMAIN}"
+  env_name="${ref_name_slug}"
+  url="https://${ref_name_slug}.${PREVIEW_ENV_DOMAIN}"
 fi
 
 echo "
-slug=${slug}
+ref_name_slug=${ref_name_slug}
 env_name=${env_name}
 url=${URL}
 " >> "${GITHUB_OUTPUT}"
