9b4c76e5a66a1a10a968919f7dd674049a8da16a
leolleocomp <leolleo.comp@gmail.com>
2023-03-06 20:10:03 +0000

    Fix infos generation

--- .github/scripts/gen_infos.sh
+++ .github/scripts/gen_infos.sh
@@ -6,8 +6,6 @@ ref_name_slug=$(\
     | sed -E -e 's#refs/[^\/]*/##;s/[^a-zA-Z0-9._-]+/-/g;s/^-*//' -e 's/-*$//' \
 )
 
-echo "infos=${ref_name_slug}" >> "${GITHUB_OUTPUT}"
-
 if [ "${ref_name_slug}" = "main" ]; then
   env_name="${LIVE_ENV_NAME}"
   url="${LIVE_ENV_URL}"
@@ -16,8 +14,6 @@ else
   url="https://${ref_name_slug}.${PREVIEW_ENV_DOMAIN}"
 fi
 
-echo "
-ref_name_slug=${ref_name_slug}
-env_name=${env_name}
-url=${URL}
-" >> "${GITHUB_OUTPUT}"
+echo "ref_name_slug=${ref_name_slug}" >> "${GITHUB_OUTPUT}"
+echo "env_name=${env_name}" >> "${GITHUB_OUTPUT}"
+echo "url=${URL}" >> "${GITHUB_OUTPUT}"
